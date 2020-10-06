90ff14d4130f82e74a1e6330f4d26b02bdfd6267
Daniel Luo <d.luo117@gmail.com>
2020-10-05 17:47:00 -0700

    Initial Commit

---
+++ CHUGGR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CHUGGR
+//
+//  Created by Daniel Edward Luo on 10/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
